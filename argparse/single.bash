e47e46681efef5576fa26a22f3767de28be11790
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 15:05:34 -0700

    Fixup arg parsing in argparse_single.

--- argparse/single.bash
+++ argparse/single.bash
@@ -1,37 +1,43 @@
 argparse_single()
 {
+    if [[ "$#" -lt 1 ]]; then
+        printf 'argparse_single(): 1 argument required but instead got %d\n' $# >&2
+        exit 111
+    fi
+
     local DIR
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-    source "$DIR/../lib/set_columns.bash"
-    source "$DIR/../lib/set_size.bash"
-
-    if [[ "$#" -ne 1 ]]; then
-        printf 'Non single argument passed to argparse single: "%s"\n' "$@"
+    if [[ "$#" -gt 1 ]]; then
+        source "$DIR/../util/join_by.bash"
+        printf 'argparse_single(): 1 argument required but instead got %d: ["%s"]\n' $# "$(join_by '", "' "$@")" >&2
         exit 111
     fi
+
+    source "$DIR/../lib/set_size.bash"
 
     case "${1,,}" in
         sm|small)
             printf 'Resizing to small(80x20)...\n'
             set_size 20 80
-            ;;
+        ;;
         md|medium)
             printf 'Resizing to medium(90x20)...\n'
             set_size 20 90
-            ;;
+        ;;
         lg|large)
             printf 'Resizing to large(100x20)...\n'
             set_size 20 100
-            ;;
+        ;;
         *)
             if [[ "$1" =~ ^[0-9]+$ ]]; then
+                source "$DIR/../lib/set_columns.bash"
                 printf 'Resizing width to %d...\n' "$1"
                 set_columns "$1"
             else
-                printf 'Unrecognized argument: "%s"\n' "$1"
+                printf 'Unrecognized argument: "%s"\n' "$1" >&2
                 exit 112
             fi
-            ;;
+        ;;
     esac
 }
