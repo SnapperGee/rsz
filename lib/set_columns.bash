b89492ac71bb2eef71dab449956494de18175ec3
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 15:09:01 -0700

    Don't quote numeric variables and print to stderr and only source util script if needed.

--- lib/set_columns.bash
+++ lib/set_columns.bash
@@ -1,25 +1,25 @@
 set_columns()
 {
-    local DIR
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-    source "$DIR/../util/join_by.bash"
-
-    if [[ "$#" -eq 0 ]]; then
-        printf 'set_columns(): requires 1 argument (a width), but got 0: []\n'
+    if [[ $# -eq 0 ]]; then
+        printf 'set_columns(): requires 1 argument (a width), but got 0: []\n' >&2
         exit 7
-    elif [[ "$#" -gt 1 ]]; then
-        printf 'set_columns(): requires 1 argument (a width), but got %d: ["%s"]\n' "$#" "$(join_by '", "' "$@")"
+    elif [[ $# -gt 1 ]]; then
+
+        local DIR
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        source "$DIR/../util/join_by.bash"
+
+        printf 'set_columns(): requires 1 argument (a width), but got %d: ["%s"]\n' $# "$(join_by '", "' "$@")" >&2
         exit 8
-    elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
-        printf 'set_columns(): Non number argument: "%s"\n' "$1"
+    elif ! [[ $1 =~ ^[0-9]+$ ]]; then
+        printf 'set_columns(): Non number argument: "%s"\n' "$1" >&2
         exit 9
-    elif [[ "$1" -lt 1 ]]; then
-        printf 'set_columns(): argument less than 1: "%d"\n' "$1"
+    elif [[ $1 -lt 1 ]]; then
+        printf 'set_columns(): argument less than 1: "%d"\n' "$1" >&2
         exit 10
     fi
 
-    if [[ "$(tput cols)" -ne "$1" ]]; then
+    if [[ "$(tput cols)" -ne $1 ]]; then
         printf '\e[8;%d;%dt' "$(tput lines)" "$1"
     fi
 }
