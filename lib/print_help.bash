3282148ab623cead45aa89c9199f30f81e3cfc56
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 14:50:02 -0700

    When using groff to print help don't use printf.

--- lib/print_help.bash
+++ lib/print_help.bash
@@ -4,8 +4,8 @@ print_help()
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
     if command -v groff &> /dev/null; then
-        printf '%s\n' "$(groff -T ascii -mdoc "$DIR/../man/rsz.1")"
+        groff -T ascii -mdoc "$DIR/../man/rsz.1"
     else
-        printf 'Non groff help command text pending implementation...'
+        printf 'Non groff help command text pending implementation...\n'
     fi
 }
