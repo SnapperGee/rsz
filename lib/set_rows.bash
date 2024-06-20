c98bb9bf4b9b9a6a3ecb6fe1250799faeb15d76b
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 15:10:47 -0700

    Don't quote numeric variables and print to stderr and only source util script if needed.

--- lib/set_rows.bash
+++ lib/set_rows.bash
@@ -1,25 +1,25 @@
 set_rows()
 {
-    local DIR
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-    source "$DIR/../util/join_by.bash"
-
-    if [[ "$#" -eq 0 ]]; then
-        printf 'set_rows(): requires 1 argument (a height), but got 0: []\n'
+    if [[ $# -eq 0 ]]; then
+        printf 'set_rows(): requires 1 argument (a height), but got 0: []\n' >&2
         exit 11
-    elif [[ "$#" -gt 1 ]]; then
-        printf 'set_rows(): requires 1 argument (a height), but got %d: ["%s"]\n' "$#" "$(join_by '", "' "$@")"
+    elif [[ $# -gt 1 ]]; then
+
+        local DIR
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        source "$DIR/../util/join_by.bash"
+
+        printf 'set_rows(): requires 1 argument (a height), but got %d: ["%s"]\n' $# "$(join_by '", "' "$@")" >&2
         exit 12
-    elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
-        printf 'set_rows(): Non number argument: "%s"\n' "$1"
+    elif ! [[ $1 =~ ^[0-9]+$ ]]; then
+        printf 'set_rows(): Non number argument: "%s"\n' "$1" >&2
         exit 13
-    elif [[ "$1" -lt 1 ]]; then
-        printf 'set_rows(): argument less than 1: "%d"\n' "$1"
+    elif [[ $1 -lt 1 ]]; then
+        printf 'set_rows(): argument less than 1: "%d"\n' "$1" >&2
         exit 14
     fi
 
-    if [[ "$(tput lines)" -ne "$1" ]]; then
+    if [[ "$(tput lines)" -ne $1 ]]; then
         printf '\e[8;%d;%dt' "$1" "$(tput cols)"
     fi
 }
