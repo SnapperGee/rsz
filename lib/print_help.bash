0352c0821fe0388e8becb6f3eddd17e440e3f60e
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 15:31:15 -0700

    Only source man page in print_help script if groff is present.

--- lib/print_help.bash
+++ lib/print_help.bash
@@ -1,9 +1,10 @@
 print_help()
 {
-    local DIR
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
     if command -v groff &> /dev/null; then
+
+        local DIR
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
         groff -T ascii -mdoc "$DIR/../man/rsz.1"
     else
         printf 'Non groff help command text pending implementation...\n'
