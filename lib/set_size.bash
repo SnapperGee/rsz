501e5c529fcae835642bafc02ea4385610bd5bc7
Snap <XxSnapperGeexX@gmail.com>
2024-06-20 15:14:42 -0700

    Don't quote numeric variables and print to stderr and only source util script if needed.

--- lib/set_size.bash
+++ lib/set_size.bash
@@ -1,37 +1,37 @@
 set_size()
 {
-    local DIR
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    if [[ $# -ne 2 ]]; then
 
-    source "$DIR/../util/join_by.bash"
+        local DIR
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        source "$DIR/../util/join_by.bash"
 
-    if [[ "$#" -ne 2 ]]; then
         local errMsg='set_size() requires 2 arguments (a width and height), but got'
 
-        case "$#" in
+        case $# in
             0)
-                printf '%s %d: %s\n' "$errMsg" "$#" '[]'
+                printf '%s %d: %s\n' "$errMsg" $# '[]'
             ;;
             1)
-                printf '%s %d: "%s"\n' "$errMsg" "$#" "$1"
+                printf '%s %d: "%s"\n' "$errMsg" $# "$1"
             ;;
             *)
-                printf '%s %d: ["%s"]\n' "$errMsg" "$#" "$(join_by '", "' "$@")"
+                printf '%s %d: ["%s"]\n' "$errMsg" $# "$(join_by '", "' "$@")"
             ;;
         esac
 
         exit 2
-    elif ! [[ "$1" =~ ^[0-9]+$ ]]; then
-        printf 'Non number passed as 1st argument to set_size(): "%s"\n' "$1"
+    elif ! [[ $1 =~ ^[0-9]+$ ]]; then
+        printf 'Non number passed as 1st argument to set_size(): "%s"\n' "$1" >&2
         exit 3
-    elif ! [[ "$2" =~ ^[0-9]+$ ]]; then
-        printf 'Non number passed as 2nd argument to set_columns: "%s"\n' "$2"
+    elif ! [[ $2 =~ ^[0-9]+$ ]]; then
+        printf 'Non number passed as 2nd argument to set_columns: "%s"\n' "$2" >&2
         exit 4
     elif [[ $1 -lt 1 ]]; then
-        printf 'set_size(): width value less than 1: "%d"\n' "$1"
+        printf 'set_size(): width value less than 1: "%d"\n' "$1" >&2
         exit 5
     elif [[ $2 -lt 1 ]]; then
-        printf 'set_size(): height value less than 1: "%d"\n' "$2"
+        printf 'set_size(): height value less than 1: "%d"\n' "$2" >&2
         exit 6
     fi
 
